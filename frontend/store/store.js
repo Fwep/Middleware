5b273e54f7507c1dee7e120f9eb669b280ccfe84
Taran Cacacho <tarancacacho@gmail.com>
2019-08-05 18:11:00 -0700

    Finish rolling custom middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
+const addLoggingtoDispatch = (store) => (next) => (action) => {
+  console.log('Before action:', store.getState());
+  console.log(`Action: ${JSON.stringify(action)}`);
+
+  console.log('Next in logger:', next);
+  next(action);
+  console.log('After action:', store.getState());
+}
+
+const logNextinDispatch = store => next => action => {
+  console.log('Next in first middleware:', next)
+  console.log(store.getState());
+  return next(action);
+}
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(logNextinDispatch, addLoggingtoDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
